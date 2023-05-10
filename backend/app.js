7c0b13f18c8b8bc2a3536291b1aebac5e920b57b
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-10 12:50:16 +0300

    Adding place specific routes and getting a place by user Id

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use('/api/places',placesRoutes)
+app.use('/api/places', placesRoutes); // => /api/places...
 
-
-app.listen(5000, () => {
-  console.log("Server running!")
-});
+app.listen(5000);

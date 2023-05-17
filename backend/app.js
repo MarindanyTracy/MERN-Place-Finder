0dd9d02ebd9bbefa02f75c9d028896c0f9ff882e
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-17 12:26:53 +0300

    Creating and Storing documents in the database plus getting places by placeID

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const placesRoutes = require('./routes/places-routes');
-const usersRoutes = require('./routes/users-routes')
-const HttpError = require('./models/http-error');
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use("/api/places", placesRoutes); // => /api/places...
 
-app.use('api/users', usersRoutes)
+app.use("api/users", usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
@@ -23,14 +25,21 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
-mongoose.connect(
-  'mongodb+srv://Marindany:Marindany@cluster0.oso02oc.mongodb.net/places?retryWrites=true&w=majority'
-).then(() => {
-  app.listen(5000)}
-).catch(err => {
-  console.log(err);
-});
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
+
+app.listen(process.env.PORT, () =>{
+  console.log("Server Running!")
+  connect();
+})
+

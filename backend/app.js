388a93418e2a0edf4c18939c15032b227743c0f5
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-19 10:45:10 +0300

    Serving images statically

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const path = require('path');
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -12,6 +14,8 @@ const app = express();
 dotenv.config();
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads','images')))
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", '*');

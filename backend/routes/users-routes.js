619ea9a3120fa222398009ddfae8ff06fd8b368d
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-19 10:02:53 +0300

    Using multer in the backend to save files

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controller");
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -10,8 +11,12 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
-  [check("name").not().isEmpty(), check("email").normalizeEmail().isEmail(),
-check('password').isLength({ min:6 })],
+  fileUpload.single('image'),
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
   usersControllers.signup
 );
 

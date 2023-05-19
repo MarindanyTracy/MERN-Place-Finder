de92d855155977dedaf5a2fe43716884143faf9c
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-19 10:28:19 +0300

    Wiring frontend and backend up

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -5,24 +5,24 @@ const MIME_TYPE_MAP = {
   'image/png': 'png',
   'image/jpeg': 'jpeg',
   'image/jpg': 'jpg'
-}
+};
 
 const fileUpload = multer({
-  limits:500000,
+  limits: 500000,
   storage: multer.diskStorage({
-    destination:(req,file,cb) => {
-      cb(null, 'uploads/images')
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/images');
     },
-    filename: (req,file,cb) => {
-      const ext = MIME_TYPE_MAP(file.mimetype);
-      cb(null, uuid() + '.' + ext)
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
     }
   }),
-  fileFilter: (req,file,cb) => {
+  fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
-    let error = isValid ? null : new Error('Invalid mime type!')
-    cb(error,isValid);
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
   }
-})
+});
 
 module.exports = fileUpload;

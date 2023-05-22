9dc7600dc92e09224ddb71f03145803bfd55a2cd
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-22 12:57:04 +0300

    Building React App

---
+++ frontend/build/precache-manifest.a80e3bb4668343bbfa96d6ad1ed0a5de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897d29ab20da00d017c9c1e4794eca73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af446d359bd5d3ac8fe",
+    "url": "/static/css/main.b07738f6.chunk.css"
+  },
+  {
+    "revision": "002a9e8a3d53208219c5",
+    "url": "/static/js/2.95fb10ae.chunk.js"
+  },
+  {
+    "revision": "1af446d359bd5d3ac8fe",
+    "url": "/static/js/main.f596e8e7.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
